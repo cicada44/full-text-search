d24b1b3f4446b78df7d0affc3f41c1660ed26754
Cicada <hubbleadd@bk.ru>
2023-08-28 10:20:53 +0600

    Complete project structure

---
+++ include/libmath/sum.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace fts {
+
+/**
+ * @brief Accumulate 2 numbers.
+ * @param a First accumulating number.
+ * @param b Second accumulating number.
+ * @return double result of accumulating.
+ */
+double accumulate(double a, double b);
+
+}  // namespace fts
---
+++ src/libmath/sum.cpp
@@ -0,0 +1,7 @@
+#include <libmath/sum.h>
+
+namespace fts {
+
+double accumulate(double a, double b) { return a + b; }
+
+}  // namespace fts
